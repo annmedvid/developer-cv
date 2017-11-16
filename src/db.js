42535629a08592c52ae018f453c5d848c5358460
Anna Medvid <anasim669@gmail.com>
2017-11-16 14:01:11 +0200

    Add fake API

---
+++ src/db.js
@@ -0,0 +1,24 @@
+const skills = [
+    {
+        name: "HTML5",
+        level: 8
+    },
+    {
+        name: "CSS3",
+        level: 5
+    },
+    {
+        name: "JavaScript",
+        level: 10
+    },
+    {
+        name: "ReactJS",
+        level: 8
+    },
+    {
+        name: "AngularJS",
+        level: 8
+    },
+]
+
+export default skills
---
+++ src/utils/fakeApi.js
@@ -0,0 +1,9 @@
+import skills from '../db.js'
+
+export const fetchSkills = () => {
+  	return skills
+}
+
+export const fetchRequest = () => {
+  	return true
+}
