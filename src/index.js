872e8021f8272a537a1d50830c89bf89585fb0f8
Anna Medvid <anasim669@gmail.com>
2017-11-16 14:09:59 +0200

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {HashRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware} from 'redux'
+import rootReducer from './rootReducer'
 import routes from './routes'
+
+let store = createStore(
+	rootReducer,
+)
+
 ReactDOM.render(
+	<Provider store={store}>
 		<HashRouter>
 			{routes}
 		</HashRouter>
+  	</Provider>,
   	document.querySelector('.app')
 )
